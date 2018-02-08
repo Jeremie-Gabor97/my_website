485b69f82dc78b99d5cf89cd885b442bc46ec5f8
Jeremie Gabor <jeremie.gabor@gmail.com>
2018-02-07 23:00:27 -0600

    intro changes

--- scripts/MainController.js
+++ scripts/MainController.js
@@ -1,4 +1,4 @@
-app.controller('MainController',['$scope',function($scope,$http){
+app.controller('MainController',['$scope',function($scope,$http,$window){
 
     
     $scope.Introductions = [{"Title":"About me","Body":"<p>I am a third-year Computer Science and Statistics student at the University of Waterloo with a passion for coding and sports. My dream one day is to combine these two passions and work in the sports analytics industry. In the mean time, I am a competitive ultimate frisbee player and I am always looking for my next big challenge.</p>","Image":"https://cdn.cnn.com/cnnnext/dam/assets/130821120202-human-to-hero-mark-poole-ultimate-00000408-horizontal-large-gallery.jpg"
