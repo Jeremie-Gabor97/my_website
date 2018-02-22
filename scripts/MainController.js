2cdb2528cb4c30a27c566919f12fa42f710951f7
Jeremie Gabor <jeremie.gabor@gmail.com>
2018-02-21 21:12:55 -0600

    links to media

--- scripts/MainController.js
+++ scripts/MainController.js
@@ -4,3 +4,7 @@ app.controller('MainController',['$scope',function($scope,$http,$window){
     $scope.Aboutme = [{"Body":"<br><p>I am a third-year Computer Science and Statistics student at the University of Waterloo with a passion for coding and sports. My dream one day is to combine these two passions and work in the sports analytics industry. In the mean time, I am a competitive ultimate frisbee player and I am always looking for my next big challenge.</p>"
     }];
 }]);
+
+    $.("#contact-button").click(function(){
+        scrollTo()
+    })
